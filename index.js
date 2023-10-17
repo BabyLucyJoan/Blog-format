c84765ba73bf8003556a83c0fcc4e3d338459e87
Lucy Nwokoye <142795917+BabyLucyJoan@users.noreply.github.com>
2023-10-17 15:36:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,146 @@
+
+
+
+const blogs0 = [
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2021/03/black-camaru-thinking-funny-laughing.jpg",
+        paragraph : "ENGLISH STUDENTS!!! None Of You All Can Win This Challenge – Prove Me Wrong"
+    },
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2023/01/davido-wizkid.jpg",
+        paragraph : "If You’re To Suggest A Cute Twins Name For Davido’s New Babies – What Names Will You Drop"
+    },
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2023/03/Zinoleesky-1-e1685548421802.jpg",
+        paragraph : "LET’S TALK!! If Zinoleesky Drop a Song Today, Will You Jam It Or He Remains Cancelled In Your Book?"
+    },
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2023/10/Love-Damini-vs-I-Told-Them.jpg",
+        paragraph : "MUSIC LOVERS!! Which Is A Better Album – “Love,Damini” Or “I Told Them”?"
+    }
+    
+    
+    
+]
+
+const blog0 = document.querySelector(".blogs")
+
+blogs0.forEach ((updates0)=>{
+    blog0.innerHTML += 
+    `<div class ="updates0">
+       <img src= "${updates0.imgUrl}">
+       <p>${updates0.paragraph}</p>
+    
+    </div>`
+}
+
+)
+
+
+
+
+const blogs1 = [
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2021/03/black-camaru-thinking-funny-laughing.jpg",
+        paragraph : "ENGLISH STUDENTS!!! None Of You All Can Win This Challenge – Prove Me Wrong"
+    },
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2023/01/davido-wizkid.jpg",
+        paragraph : "If You’re To Suggest A Cute Twins Name For Davido’s New Babies – What Names Will You Drop"
+    },
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2023/03/Zinoleesky-1-e1685548421802.jpg",
+        paragraph : "LET’S TALK!! If Zinoleesky Drop a Song Today, Will You Jam It Or He Remains Cancelled In Your Book?"
+    },
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2023/10/Love-Damini-vs-I-Told-Them.jpg",
+        paragraph : "MUSIC LOVERS!! Which Is A Better Album – “Love,Damini” Or “I Told Them”?"
+    }    
+]
+
+const blog1 = document.querySelector(".blogs")
+
+blogs1.forEach ((updates1)=>{
+    blog1.innerHTML += 
+    `<div class ="updates1">
+       <img src= "${updates1.imgUrl}">
+       <p>${updates1.paragraph}</p>
+    
+    </div>`
+}
+
+)
+
+
+const blogs2 = [
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2021/03/black-camaru-thinking-funny-laughing.jpg",
+        paragraph : "ENGLISH STUDENTS!!! None Of You All Can Win This Challenge – Prove Me Wrong"
+    },
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2023/01/davido-wizkid.jpg",
+        paragraph : "If You’re To Suggest A Cute Twins Name For Davido’s New Babies – What Names Will You Drop"
+    },
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2023/03/Zinoleesky-1-e1685548421802.jpg",
+        paragraph : "LET’S TALK!! If Zinoleesky Drop a Song Today, Will You Jam It Or He Remains Cancelled In Your Book?"
+    },
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2023/10/Love-Damini-vs-I-Told-Them.jpg",
+        paragraph : "MUSIC LOVERS!! Which Is A Better Album – “Love,Damini” Or “I Told Them”?"
+    }    
+]
+
+const blog2 = document.querySelector(".blogs")
+
+blogs2.forEach ((updates2)=>{
+    blog2.innerHTML += 
+    `<div class ="updates2">
+       <img src= "${updates2.imgUrl}">
+       <p>${updates2.paragraph}</p>
+    
+    </div>`
+}
+
+)
+
+
+
+
+const blogs3 = [
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2021/03/black-camaru-thinking-funny-laughing.jpg",
+        paragraph : "ENGLISH STUDENTS!!! None Of You All Can Win This Challenge – Prove Me Wrong"
+    },
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2023/01/davido-wizkid.jpg",
+        paragraph : "If You’re To Suggest A Cute Twins Name For Davido’s New Babies – What Names Will You Drop"
+    },
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2023/03/Zinoleesky-1-e1685548421802.jpg",
+        paragraph : "LET’S TALK!! If Zinoleesky Drop a Song Today, Will You Jam It Or He Remains Cancelled In Your Book?"
+    },
+    {
+        imgUrl : "https://www.naijaloaded.com.ng/wp-content/uploads/2023/10/Love-Damini-vs-I-Told-Them.jpg",
+        paragraph : "MUSIC LOVERS!! Which Is A Better Album – “Love,Damini” Or “I Told Them”?"
+    }    
+]
+
+const blog3 = document.querySelector(".blogs")
+
+blogs3.forEach ((updates3)=>{
+    blog3.innerHTML += 
+    `<div class ="updates3">
+       <img src= "${updates3.imgUrl}">
+       <p>${updates3.paragraph}</p>
+    
+    </div>`
+}
+
+)
+
+
+
+  const allBody= document.querySelector("body")
+  allBody.classList.toggle("darkTheme")
+
